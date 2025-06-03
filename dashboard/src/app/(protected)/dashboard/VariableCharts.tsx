6ce166d199570e6058d3fe3ee2d75952b93ba329
Mariano Dominguez <mardom4164@gmail.com>
2025-06-03 10:58:57 -0300

    add more info

--- dashboard/src/app/(protected)/dashboard/VariableCharts.tsx
+++ dashboard/src/app/(protected)/dashboard/VariableCharts.tsx
@@ -307,6 +307,19 @@ const VariableCharts: React.FC<VariableChartsProps> = ({ measurements, selectedP
                 <h3 className="text-lg font-semibold mb-4 flex items-center justify-between">
                   <span>{variable}</span>
                   <div className="flex items-center">
+                    {correctPercentage >= 80 ? (
+                      <svg className="w-5 h-5 text-green-500 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                      </svg>
+                    ) : correctPercentage >= 50 ? (
+                      <svg className="w-5 h-5 text-yellow-500 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd"></path>
+                      </svg>
+                    ) : (
+                      <svg className="w-5 h-5 text-red-500 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                      </svg>
+                    )}
                     <span className="text-sm font-medium ml-2 px-2 py-1 bg-gray-100 rounded-full">
                       {correctPercentage}% correcto ({correctCount}/{totalCount})
                     </span>
@@ -669,6 +682,19 @@ const VariableCharts: React.FC<VariableChartsProps> = ({ measurements, selectedP
               <h3 className="text-lg font-semibold mb-4 flex items-center justify-between">
                 <span>{variable}</span>
                 <div className="flex items-center">
+                  {correctPercentage >= 80 ? (
+                    <svg className="w-5 h-5 text-green-500 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                    </svg>
+                  ) : correctPercentage >= 50 ? (
+                    <svg className="w-5 h-5 text-yellow-500 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd"></path>
+                    </svg>
+                  ) : (
+                    <svg className="w-5 h-5 text-red-500 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                  )}
                   <span className="text-sm font-medium ml-2 px-2 py-1 bg-gray-100 rounded-full">
                     {correctPercentage}% correcto ({correctCount}/{totalCount})
                   </span>
