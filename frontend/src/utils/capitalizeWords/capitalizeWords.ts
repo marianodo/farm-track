7902456188e3da378caffda218cab60c6d6b8699
Mariano Dominguez <mardom4164@gmail.com>
2025-06-05 12:20:55 -0300

    fix gradle

--- frontend/src/utils/capitalizeWords/capitalizeWords.ts
+++ frontend/src/utils/capitalizeWords/capitalizeWords.ts
@@ -1,6 +1,8 @@
-export default function capitalizeWords(str: string): string {
+export default function capitalizeWords(str?: string | null): string {
+  if (!str) return '';
   return str
     .split(' ') // Divide el string en un array por espacios
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Capitaliza la primera letra de cada palabra
     .join(' '); // Une el array en un solo string
 }
+
