dbbcd6e67d312d5c44f51a11f5facaf2902e9aa0
Mariano Dominguez <mardom4164@gmail.com>
2025-10-22 18:05:15 -0300

    fix update

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,10 +1,10 @@
 import * as Localization from 'expo-localization';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import translationEn from './locales/en-US/translation.json';
 import translationEs from './locales/es-ES/translation.json';
+import { safeAsyncStorage } from '../utils/asyncStorageHelper';
 
 //Este archivo es usado para almacenar el lenguaje que seleccione el usuario si asi sucede y hacer que persista. En el caso que no seleccione un idioma tomaria el por defecto de su dispositivo.
 
@@ -14,7 +14,8 @@ const resources = {
 };
 
 const initI18n = async () => {
-  let savedLanguage = await AsyncStorage.getItem('language');
+  let savedLanguage = await safeAsyncStorage.getItem('language');
+  
   if (!savedLanguage) {
     savedLanguage = Localization.getLocales()[0].languageTag;
   }
@@ -30,6 +31,20 @@ const initI18n = async () => {
   });
 };
 
-initI18n();
+// Solo inicializar si no estamos en contexto de servidor
+if (typeof window !== 'undefined') {
+  initI18n();
+} else {
+  // Inicialización básica para servidor/web
+  i18n.use(initReactI18next).init({
+    compatibilityJSON: 'v3',
+    resources,
+    lng: 'es-ES',
+    fallbackLng: 'es-ES',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+}
 
 export default i18n;
